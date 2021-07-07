9319f7b98146497af838290ee6584f99bebd286f
mktwohy <69822801+mktwohy@users.noreply.github.com>
2021-07-07 17:23:01 -0500

    PianoView draws correctly after refactor

--- app/src/main/java/com/example/synth/PianoView.kt
+++ app/src/main/java/com/example/synth/PianoView.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.res.Resources
 import android.graphics.*
 import android.util.AttributeSet
-import android.view.MotionEvent
+import android.util.Log
 import android.view.View
 
 data class Key(
@@ -22,7 +22,6 @@ data class PianoGrid(
 class PianoView(context: Context, attrs: AttributeSet)
     : View(context, attrs) {
 
-
     private val screen = (Resources.getSystem().displayMetrics)
     private val whiteWidth = screen.widthPixels / 7.toFloat()
     private val whiteHeight = screen.heightPixels.toFloat()
@@ -30,27 +29,35 @@ class PianoView(context: Context, attrs: AttributeSet)
     private val blackHeight = whiteHeight * 0.55f
 
     private val white = Paint().apply { setARGB(255, 255, 255, 255) }
+    private val purple = Paint().apply { setARGB(255, 255, 0, 255) }
     private val black = Paint().apply {
         setARGB(255, 0, 0, 0)
         strokeWidth = whiteWidth / 80
     }
-    private val purple = Paint().apply { setARGB(255, 255, 0, 255) }
 
+    private val keys: List<Key>
+    private val pianoGrid: PianoGrid
 
-    private val keys = initKeys(4 * 12 until 5 * 12)
-    private fun initKeys(noteRange: IntRange) =
-        mutableListOf<Key>().apply {
+    init{
+        keys = createKeys(4 * 12..5 * 12)
+        pianoGrid = createGrid()
+        assignRectsToKeys()
+    }
+
+    private fun createKeys(noteRange: IntRange): List<Key>{
+        Log.d("m_funCall","InitKeys!")
+        val kList = mutableListOf<Key>().apply {
             val notes = Note.values().toList().subList(noteRange.first, noteRange.last)
             for (n in notes) {
-                this.add(
-                    Key(n, SinSignal(n.freq))
-                )
-                toList()
+                this.add(Key(n, SinSignal(n.freq)))
             }
         }
+        return kList.toList()
+    }
 
-    private val pianoGrid = initGrid()
-    private fun initGrid(): PianoGrid {
+
+    private fun createGrid(): PianoGrid {
+        Log.d("m_funCall","initGrid!")
         fun initRow(top: Float, bottom: Float, widths: List<Float>): List<RectF> {
             val rects = mutableListOf<RectF>()
             var left = 0f
@@ -58,17 +65,18 @@ class PianoView(context: Context, attrs: AttributeSet)
                 rects.add(
                     RectF(left, top, left + width, bottom)
                 )
+                left += width
             }
             return rects.toList()
         }
 
         val topWidths = mutableListOf<Float>().apply {
-            add(whiteWidth * 3 / 4)
-            repeat(3) { add(whiteWidth * 3 / 4) }
-            repeat(2) { add(blackWidth) }
-            repeat(2) { add(whiteWidth * 3 / 4) }
+            val topWhiteWidth = whiteWidth * 3 / 4
+            add(topWhiteWidth)
+            repeat(3) { add(blackWidth) }
+            repeat(2) { add(topWhiteWidth) }
             repeat(5) { add(blackWidth) }
-            add(whiteWidth * 3 / 4)
+            add(topWhiteWidth)
             toList()
         }
 
@@ -78,26 +86,26 @@ class PianoView(context: Context, attrs: AttributeSet)
         }
 
         return PianoGrid(
-            initRow(0f, whiteHeight, topWidths),
-            initRow(whiteHeight, screen.heightPixels.toFloat(), bottomWidths)
+            initRow(0f, blackHeight, topWidths),
+            initRow(blackHeight, whiteHeight, bottomWidths)
         )
     }
 
-    private fun assignRectsToKeys(pg: PianoGrid) {
+    private fun assignRectsToKeys() {
+        Log.d("m_funCall","assignRectsToKeys!")
         fun assignWhite(key: Key, topRowIndex: Int, bottomRowIndex: Int) {
             key.rects.apply {
-                add(pg.bottomRow[bottomRowIndex])
-                add(pg.topRow[topRowIndex])
-
+                add(pianoGrid.bottomRow[bottomRowIndex])
+                add(pianoGrid.topRow[topRowIndex])
             }
         }
 
         fun assignBlack(key: Key, topRowIndex: Int) {
-            key.rects.add(pg.topRow[topRowIndex])
+            key.rects.add(pianoGrid.topRow[topRowIndex])
         }
 
         for (k in keys) {
-            when (k.name.toString().substring(0, 1)) {
+            when (k.name.toString().substring(0, 2)) {
                 "C_" -> assignWhite(k, 0, 0)
                 "Cs" -> assignBlack(k, 1)
                 "D_" -> assignWhite(k, 2, 1)
@@ -111,22 +119,22 @@ class PianoView(context: Context, attrs: AttributeSet)
                 "As" -> assignBlack(k, 10)
                 "B_" -> assignWhite(k, 11, 6)
             }
-
         }
     }
 
     override fun onDraw(canvas: Canvas?) {
+        Log.d("m_funCall","onDraw!")
         super.onDraw(canvas)
         if (canvas == null) return
 
-        assignRectsToKeys(pianoGrid)
-        keys.forEach {
-            for (rect in it.rects) {
-                if (it.name.toString()[1] == '_') {
-                    canvas.drawRect(rect, white)
-                } else {
-                    canvas.drawRect(rect, black)
-                }
+        for (k in keys) {
+            for (rect in k.rects) {
+                if (k.name.toString()[1] == '_')
+                    canvas.apply {
+                        drawRect(rect, white)
+                        drawLine(rect.left, rect.top, rect.left, rect.bottom, black)
+                    }
+                else canvas.drawRect(rect, black)
             }
         }
     }
