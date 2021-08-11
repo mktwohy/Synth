57367878e0e52631f8caa956718fb60987051dcf
mktwohy <mktwohy@gmail.com>
2021-08-11 14:46:57 -0500

    Conversion to Jetpack Compose

--- app/src/androidTest/java/com/example/synth/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/synth/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.synth
+package com.example.synthjetpackcompose
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.synth", appContext.packageName)
+        assertEquals("com.example.synthjetpackcompose", appContext.packageName)
     }
 }
