0d2a8bc66106d2d98f3096effe849d9e204991df
mktwohy <69822801+mktwohy@users.noreply.github.com>
2021-08-13 19:47:12 -0500

    Renamed HarmonicSlider to VolumeSlider

--- app/src/main/java/com/example/synth/HarmonicSeries.kt
+++ app/src/main/java/com/example/synth/HarmonicSeries.kt
@@ -3,7 +3,6 @@ package com.example.synth
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Slider
-import androidx.compose.material.SliderColors
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -11,13 +10,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.synth.ui.theme.Purple200
-import com.example.synth.ui.theme.Purple500
-import com.example.synth.ui.theme.Purple700
 
 
 @Composable
-fun HarmonicScreen(modifier: Modifier = Modifier, initialValue: Float){
+fun VolumeSliderScreen(modifier: Modifier = Modifier, initialValue: Float){
     var amplitude by remember { mutableStateOf(initialValue) }
 
     Row(
@@ -38,7 +34,7 @@ fun HarmonicScreen(modifier: Modifier = Modifier, initialValue: Float){
                 color = Color.White
             )
         }
-        HarmonicSlider(
+        VolumeSlider(
             modifier = Modifier.border(width = 10.dp, color = Color.White),
             value = amplitude,
             onValueChange = { amplitude = it }
@@ -49,7 +45,7 @@ fun HarmonicScreen(modifier: Modifier = Modifier, initialValue: Float){
 
 
 @Composable
-fun HarmonicSlider(
+fun VolumeSlider(
     modifier: Modifier = Modifier,
     value: Float,
     onValueChange: (Float) -> Unit
--- app/src/main/java/com/example/synth/MainActivity.kt
+++ app/src/main/java/com/example/synth/MainActivity.kt
@@ -5,14 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 //            XYPlotScreen()
-            HarmonicScreen(modifier = Modifier.fillMaxHeight(), initialValue = 0f)
+            VolumeSliderScreen(modifier = Modifier.fillMaxHeight(), initialValue = 0f)
         }
     }
 }
