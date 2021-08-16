cda38de00704a02cf9319ba4266a97da5d498a46
mktwohy <69822801+mktwohy@users.noreply.github.com>
2021-08-16 12:57:26 -0500

    Some more attempts at VolumeSlider

--- app/src/main/java/com/example/synth/HarmonicSeries.kt
+++ app/src/main/java/com/example/synth/HarmonicSeries.kt
@@ -8,41 +8,100 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun VolumeSliderScreen(modifier: Modifier = Modifier, initialValue: Float){
+fun VolumeSliderScreen(modifier: Modifier = Modifier, initialValue: Float = 0f){
     var amplitude by remember { mutableStateOf(initialValue) }
 
-    Row(
-        modifier = modifier
-            .rotate(-90f)
-            .wrapContentSize()
-            .border(width = 2.dp, color = Color.White),
-        verticalAlignment = Alignment.CenterVertically
-    ){
-        Box(
-            modifier = Modifier
-                .fillMaxWidth(0.1f)
-                .rotate(90f),
-            contentAlignment = Alignment.Center
-        ){
-            Text(
-                text = (amplitude*100).toInt().toString(),
-                color = Color.White
-            )
-        }
-        VolumeSlider(
-            modifier = Modifier.border(width = 10.dp, color = Color.White),
-            value = amplitude,
-            onValueChange = { amplitude = it }
-        )
-    }
+    VolumeSlider(
+        modifier = modifier,
+        value = amplitude,
+        onValueChange = { amplitude = it }
+    )
 }
 
 
+
+@Composable
+fun VolumeSliderOLD(
+    modifier: Modifier = Modifier,
+    value: Float,
+    onValueChange: (Float) -> Unit
+){
+
+    Column(
+        modifier = modifier.border(width = 2.dp, color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Slider(
+            modifier = Modifier
+                .rotate(-90f)
+                .fillMaxHeight(0.9f)
+                .border(width = 10.dp, color = Color.White),
+            value = value,
+            onValueChange = onValueChange,
+        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.End)
+                .fillMaxHeight(0.1f),
+            contentAlignment = Alignment.Center
+        ){
+            Text(
+                text = (value*100).toInt().toString(),
+                color = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun VolumeSlider2(
+    modifier: Modifier = Modifier,
+    value: Float,
+    onValueChange: (Float) -> Unit
+){
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(0.9f)
+                .fillMaxWidth()
+                .border(width = 2.dp, color = Color.White)
+        ) {
+            Slider(
+                modifier = Modifier
+                    .rotate(-90f)
+                    .fillMaxSize()
+                    .border(width = 3.dp, color = Color.Red)
+                ,
+                value = value,
+                onValueChange = onValueChange,
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .border(width = 2.dp, color = Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = (value*100).toInt().toString(),
+                color = Color.White
+            )
+        }
+
+    }
+}
 
 @Composable
 fun VolumeSlider(
@@ -50,9 +109,46 @@ fun VolumeSlider(
     value: Float,
     onValueChange: (Float) -> Unit
 ){
-    Slider(
-        modifier = modifier,
-        value = value,
-        onValueChange = onValueChange,
-    )
+
+    Box(modifier = modifier.border(width = 2.dp, color = Color.Red)){
+        Row(
+            modifier = Modifier
+                .rotate(90f)
+                .fillMaxSize()
+                .border(width = 2.dp, color = Color.Green),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(0.9f)
+                    .fillMaxWidth()
+                    .border(width = 2.dp, color = Color.White)
+            ) {
+                Slider(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .border(width = 3.dp, color = Color.Red)
+                    ,
+                    value = value,
+                    onValueChange = onValueChange,
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .rotate(-90f)
+                    .fillMaxSize()
+                    .border(width = 2.dp, color = Color.White),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = (value*100).toInt().toString(),
+                    color = Color.White
+                )
+            }
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/synth/MainActivity.kt
+++ app/src/main/java/com/example/synth/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            XYPlotScreen()
-            VolumeSliderScreen(modifier = Modifier.fillMaxHeight(), initialValue = 0f)
+            Row {
+                repeat(5){
+                    VolumeSliderScreen(modifier = Modifier.fillMaxHeight().width(50.dp))
+                }
+
+            }
+
         }
     }
 }
