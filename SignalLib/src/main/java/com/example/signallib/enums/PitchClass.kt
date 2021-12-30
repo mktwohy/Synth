0b2501880633009a2c21e7fc9416ba798a5b0370
mktwohy <69822801+mktwohy@users.noreply.github.com>
2021-12-29 21:43:49 -0600

    aar version 1.2.0-alpha

--- SignalLib/src/main/java/com/example/signallib/enums/PitchClass.kt
+++ SignalLib/src/main/java/com/example/signallib/enums/PitchClass.kt
@@ -1,3 +1,9 @@
 package com.example.signallib.enums
 
-enum class PitchClass { C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B }
+enum class PitchClass {
+    C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B;
+
+    companion object{
+        val classes = values().toList()
+    }
+}
