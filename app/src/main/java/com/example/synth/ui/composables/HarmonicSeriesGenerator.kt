897cf7bc8e883ba00953419fc7774783639e7a1f
mktwohy <69822801+mktwohy@users.noreply.github.com>
2022-01-11 21:01:32 -0600

    created Dial composable

---
+++ app/src/main/java/com/example/synth/ui/composables/HarmonicSeriesGenerator.kt
@@ -0,0 +1,110 @@
+package com.example.synth.ui.composables
+
+import android.view.MotionEvent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInWindow
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.sp
+import com.example.synth.viewModels.HarmonicSeriesGeneratorViewModel
+import kotlin.math.PI
+import kotlin.math.atan2
+
+@Composable
+fun HarmonicSeriesGenerator(
+    modifier: Modifier,
+    viewModel: HarmonicSeriesGeneratorViewModel
+){
+
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun TestDial(){
+    var dialState by remember { mutableStateOf(0f) }
+    Text(text = dialState.toString(), fontSize = 24.sp, color = Color.White)
+    
+    Dial(
+        modifier = Modifier.fillMaxSize(),
+        value = dialState,
+        onValueChange = { dialState = it }
+    )
+}
+
+@ExperimentalComposeUiApi
+@Composable
+/** Based off of [Phillip Lackner's music knob](https://www.youtube.com/watch?v=TOflUdgx4pw) */
+private fun Dial(
+    modifier: Modifier,
+    lineColor: Color = Color.White,
+    borderColor: Color = Color.Gray,
+    innerColor: Color = Color.DarkGray,
+    value: Float,
+    onValueChange: (Float) -> Unit
+){
+    var touchX by remember { mutableStateOf(0f) }
+    var touchY by remember { mutableStateOf(0f) }
+    var centerX by remember { mutableStateOf(0f) }
+    var centerY by remember { mutableStateOf(0f) }
+
+    Canvas(
+        modifier = modifier
+            .onGloballyPositioned {
+                val windowBounds = it.boundsInWindow() //if reusing, might need to change this
+                centerX = windowBounds.size.width / 2f
+                centerY = windowBounds.size.height / 2f
+            }
+            .pointerInteropFilter { event ->
+                touchX = event.x
+                touchY = event.y
+                val angle = -atan2(centerX - touchX, centerY - touchY) * (180 / PI).toFloat()
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN,
+                    MotionEvent.ACTION_MOVE -> {
+                        val posAngle = toPositiveAngle(angle)
+
+                        val percent = (if (posAngle < 0f) posAngle + 180f else posAngle) / 360f
+
+                        onValueChange(percent)
+                        true
+                    }
+                    else -> false
+                }
+            }
+    ){
+        val radius = this.size.minDimension/2f
+        val innerRadius = radius * 0.95f
+
+        rotate(toPositiveAngle(value * 360f)){
+            drawCircle(
+                color = borderColor,
+                radius = radius
+            )
+            drawCircle(
+                color = innerColor,
+                radius = innerRadius
+            )
+            drawLine(
+                color = lineColor,
+                strokeWidth = 4f,
+                start = this.center,
+                end = Offset(radius, center.y - innerRadius)
+            )
+        }
+    }
+
+}
+
+
+private fun toPositiveAngle(angle: Float) =
+    if (angle in 0f..180f) angle
+    else 360f + angle
--- app/src/main/java/com/example/synth/ui/composables/Main.kt
+++ app/src/main/java/com/example/synth/ui/composables/Main.kt
@@ -17,58 +17,60 @@ import com.example.synth.AppModel
 @ExperimentalComposeUiApi
 @Composable
 fun Main(isPortrait: Boolean) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        HarmonicSeriesEditor(
-            modifier = Modifier
-                .fillMaxHeight(if (isPortrait) 0.5f else 0.3f)
-                .border(1.dp, Color.White),
-            viewModel = AppModel.harmonicSeriesViewModel
-        )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.4f)
-                .border(1.dp, Color.White)
-        ) {
-            SignalPlot(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth(0.9f)
-                    .background(Color.Black)
-                    .border(1.dp, Color.White),
-                viewModel = AppModel.SignalPlotViewModel,
-                color = Color(0.4f, 0.0f, 1f, 1f),
-                strokeWidth = 5f
-            )
-            WaveShapeSelector(
-                modifier = Modifier
-                    .fillMaxSize(),
-                viewModel = AppModel.waveFormChangeViewModel
-            )
-        }
-
-        Box(
-            Modifier
-                .fillMaxHeight(0.2f)
-                .fillMaxWidth(0.85f)) {
-            NoteRange(
-                viewModel = AppModel.pianoViewModel
-            )
-        }
-
-        Row(Modifier.border(1.dp, Color.White)) {
-            Piano(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .fillMaxWidth(if (isPortrait) 0.9f else 0.95f),
-                viewModel = AppModel.pianoViewModel,
-            )
-            PitchBend(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .border(1.dp, Color.White),
-                //viewModel = AppModel.pitchBendViewModel
-            )
-        }
-    }
+    TestDial()
+//    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+//
+//        HarmonicSeriesEditor(
+//            modifier = Modifier
+//                .fillMaxHeight(if (isPortrait) 0.5f else 0.3f)
+//                .border(1.dp, Color.White),
+//            viewModel = AppModel.harmonicSeriesViewModel
+//        )
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .fillMaxHeight(0.4f)
+//                .border(1.dp, Color.White)
+//        ) {
+//            SignalPlot(
+//                modifier = Modifier
+//                    .fillMaxHeight()
+//                    .fillMaxWidth(0.9f)
+//                    .background(Color.Black)
+//                    .border(1.dp, Color.White),
+//                viewModel = AppModel.SignalPlotViewModel,
+//                color = Color(0.4f, 0.0f, 1f, 1f),
+//                strokeWidth = 5f
+//            )
+//            WaveShapeSelector(
+//                modifier = Modifier
+//                    .fillMaxSize(),
+//                viewModel = AppModel.waveFormChangeViewModel
+//            )
+//        }
+//
+//        Box(
+//            Modifier
+//                .fillMaxHeight(0.2f)
+//                .fillMaxWidth(0.85f)) {
+//            NoteRange(
+//                viewModel = AppModel.pianoViewModel
+//            )
+//        }
+//
+//        Row(Modifier.border(1.dp, Color.White)) {
+//            Piano(
+//                modifier = Modifier
+//                    .fillMaxHeight()
+//                    .fillMaxWidth(if (isPortrait) 0.9f else 0.95f),
+//                viewModel = AppModel.pianoViewModel,
+//            )
+//            PitchBend(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .border(1.dp, Color.White),
+//                //viewModel = AppModel.pitchBendViewModel
+//            )
+//        }
+//    }
 }
