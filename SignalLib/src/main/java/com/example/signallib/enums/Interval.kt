56bab30105bd9e959bd772389c5f67a63bfa9d26
mktwohy <mktwohy@gmail.com>
2022-02-13 18:28:38 -0600

    Interval's ratio is a float, not a Rational, since Rational only works when running on Android, not in PC console

--- SignalLib/src/main/java/com/example/signallib/enums/Interval.kt
+++ SignalLib/src/main/java/com/example/signallib/enums/Interval.kt
@@ -3,17 +3,17 @@ package com.example.signallib.enums
 import android.util.Rational
 
 /** Musical intervals and their associated mathematical ratio */
-enum class Interval(val ratio: Rational){
-    PER_1   (Rational(1, 1)),
-    MIN_2   (Rational(16, 15)),
-    MAJ_2   (Rational(9, 8)),
-    MIN_3   (Rational(6, 5)),
-    MAJ_3   (Rational(5, 4)),
-    PER_4   (Rational(4, 3)),
-    TRITONE (Rational(45, 32)),
-    PER_5   (Rational(3,2)),
-    MIN_6   (Rational(8,5)),
-    MIN_7   (Rational(9,5)),
-    MAJ_7   (Rational(15,8)),
-    OCTAVE  (Rational(2,1));
+enum class Interval(val ratio: Float){
+    PER_1   (1f/1f),
+    MIN_2   (16f / 15f),
+    MAJ_2   (9f / 8f),
+    MIN_3   (6f / 5f),
+    MAJ_3   (5f / 4f),
+    PER_4   (4f / 3f),
+    TRITONE (45f / 32f),
+    PER_5   (3f /2f),
+    MIN_6   (8f /5f),
+    MIN_7   (9f /5f),
+    MAJ_7   (15f /8f),
+    OCTAVE  (2f /1f);
 }
